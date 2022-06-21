d9693e2baf238db3774906438d48d5361a1202d9
roshansw <roshanwalzade2@gmail.com>
2022-06-21 16:47:18 +0530

    Insert and Search

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -1,27 +1,26 @@
 package com.bridgelabz.linkedlist;
 
-public class LinkedList
-{
+public class LinkedList {
     Node head, tail;
-    public void add(int data)
-    {
+
+    public void add(int data) {
         Node newNode = new Node(data);
-        if(head == null)
+        if (head == null)
             head = newNode;
         else
             tail.next = newNode;
         tail = newNode;
     }
-    public void display()
-    {
+
+    public void display() {
         Node temp = head;
         while (temp != null)
         {
-            System.out.print(temp.key+"->");
+            System.out.print(temp.key + "->");
             temp = temp.next;
         }
     }
-    
+
     public void push(int data) {
         Node newNode = new Node(data);
         if (head == null) {
@@ -31,5 +30,23 @@ public class LinkedList
             newNode.next = head;
             head = newNode;
         }
+    }
+    public void insertAfter(int prevData,int data){
+        Node newNode = new Node(data);
+        Node searchedNode = this.search(prevData);
+        if( searchedNode != null){
+            newNode.next = searchedNode.next;
+            searchedNode.next = newNode;
+            //newNode.next = tempNode;
+        }
+    }
+    public Node search(int data){
+        Node temp = head;
+        while(temp != null){
+            if(temp.key == data)
+                return temp;
+            temp = temp.next;
+        }
+        return null;
     }
 }
