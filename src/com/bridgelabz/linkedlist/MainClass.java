d4dbf065d9e79c91369878bcdb1cbb6f2d734ef4
roshansw <roshanwalzade2@gmail.com>
2022-06-21 13:42:33 +0530

    added the push()

--- src/com/bridgelabz/linkedlist/MainClass.java
+++ src/com/bridgelabz/linkedlist/MainClass.java
@@ -10,9 +10,12 @@ public class MainClass
         System.out.println("Type three integer");
         int x1 = input.nextInt(), x2=input.nextInt(), x3=input.nextInt();
         LinkedList list = new LinkedList();
-        list.add(x1);
-        list.add(x2);
-        list.add(x3);
+//        list.add(x1);
+//        list.add(x2);
+//        list.add(x3);
+        list.push(x1);
+        list.push(x2);
+        list.push(x3);        
         list.display();
     }
 }
